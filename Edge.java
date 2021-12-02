b473831c2aec0522d7f716f1740b2b10adf4d63c
sareseef14 <95385314+sareseef14@users.noreply.github.com>
2021-12-02 15:16:30 +0200

    Add files via upload

--- Edge.java
+++ Edge.java
@@ -1,53 +1,67 @@
 package api;
 
-public class Edge implements  EdgeData{
-    private int id;
-    private int dest;
-    private double Weight;
-    private  String Info;
-    private int Tag;
+import javax.swing.*;
+import java.awt.*;
 
+public class Edge implements EdgeData{
 
-    public Edge(int id, int dest, double Weight, String Info,int Tag) {
-        this.id = id;
+ private int src;
+ private int dest ;
+ private double Weight;
+ private int Tag;
+ private String Info;
+    Node node;
+/*
+    constructor the details of parameters
+    just you  need to check of initialization of 'Info'
+    */
+    public Edge(int src, int dest,double Weight) {
+        this.src=src;
         this.dest=dest;
         this.Weight=Weight;
-        this.Info=Info;
-        this.Tag=Tag;
+       // this.Tag=Tag; --------> these set him and don't delete we will do talk about him
+        this.Info=" ";
     }
-
+        //return the src
     @Override
     public int getSrc() {
-        return id;
+        return src;
     }
-
+    //return dest
     @Override
     public int getDest() {
         return dest;
     }
-
+    //return weight, if this smallest from 0 then throw exception
     @Override
     public double getWeight() {
+        if (Weight<0)
+        {
+            throw new ArithmeticException("The Weight is  Equal to Zero or Bigger Than ");
+        }
         return Weight;
     }
 
+    //string represent the details of Edge and the Tag of the Node
+    //just look of node.getTag if this method return
     @Override
     public String getInfo() {
-        return Info;
+        return ""+ src +"" +dest +""+ Weight +"" + node.getTag() + "";
     }
 
     @Override
     public void setInfo(String s) {
-        this.Info=s;
+    this.Info=s;
     }
-
+    //there no idea what i need here ):
     @Override
     public int getTag() {
-        return Tag;
+    return 0;
+            //Color.getColor(Tag);
     }
 
     @Override
     public void setTag(int t) {
     this.Tag=t;
-    }
+       }
 }
