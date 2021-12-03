56a365e6984dd0a622177e9eab3409176294f2ec
SimchaTeich <79285421+SimchaTeich@users.noreply.github.com>
2021-12-03 11:08:29 +0200

    complete Edge class

--- Edge.java
+++ Edge.java
@@ -5,63 +5,58 @@ import java.awt.*;
 
 public class Edge implements EdgeData{
 
- private int src;
- private int dest ;
- private double Weight;
- private int Tag;
- private String Info;
-    Node node;
-/*
-    constructor the details of parameters
-    just you  need to check of initialization of 'Info'
-    */
-    public Edge(int src, int dest,double Weight) {
-        this.src=src;
-        this.dest=dest;
-        this.Weight=Weight;
-       // this.Tag=Tag; --------> these set him and don't delete we will do talk about him
-        this.Info=" ";
-    }
-        //return the src
-    @Override
-    public int getSrc() {
-        return src;
-    }
-    //return dest
-    @Override
-    public int getDest() {
-        return dest;
-    }
-    //return weight, if this smallest from 0 then throw exception
-    @Override
-    public double getWeight() {
-        if (Weight<0)
-        {
-            throw new ArithmeticException("The Weight is  Equal to Zero or Bigger Than ");
-        }
-        return Weight;
-    }
-
+    private Node _src;
+    private Node _dst;
+ 
+    // have to be a positive value!
+    private double _weight;
+ 
     //string represent the details of Edge and the Tag of the Node
     //just look of node.getTag if this method return
+    private String _info; //""+ src +"" +dest +""+ Weight +"" + node.getTag() + ""
+    
+    private int _tag;
+
+    public Edge(Node src, Node dest, double weight) {
+        this._src = src;
+        this._dst = dst;
+        this._weight = weight;
+        this._tag = 0;
+        this._info = "";
+    }
+    
+    @Override
+    public int getSrc() {
+        return _src.getKey();
+    }
+    
+    @Override
+    public int getDest() {
+        return _dst.getKey();
+    }
+   
+    @Override
+    public double getWeight() {
+        return _weight;
+    }
+
     @Override
     public String getInfo() {
-        return ""+ src +"" +dest +""+ Weight +"" + node.getTag() + "";
+        return _info;
     }
 
     @Override
     public void setInfo(String s) {
-    this.Info=s;
+        this.info = s;
     }
-    //there no idea what i need here ):
+ 
     @Override
     public int getTag() {
-    return 0;
-            //Color.getColor(Tag);
+        return _tag;
     }
 
     @Override
     public void setTag(int t) {
-    this.Tag=t;
-       }
+        this._tag = t;
+    }
 }
