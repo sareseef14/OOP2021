e54dfa23f7974e312f057074f8e4dc9dcda6fb8a
sareseef14 <95385314+sareseef14@users.noreply.github.com>
2021-12-07 22:07:18 +0200

    Add files via upload

--- GraphAlgo.java
+++ GraphAlgo.java
@@ -1,72 +1,101 @@
 package api;
 
-import java.security.Key;
-import java.util.Iterator;
-import java.util.List;
+import java.util.*;
 
-public class GraphAlgo implements DirectedWeightedGraphAlgorithms{
-
-    private DirectedWeightedGraph _g ;
-
+public class GraphAlgo implements DirectedWeightedGraphAlgorithms {
+    final int WHITE = 0;
+    final int GRAY = 1;
+    final int BLACK = 2;
+    private DirectedWeightedGraph _g;
+    //  private static ArrayList<EdgeData>[] adj =new ArrayList[];//
+    //private static ArrayList<EdgeData>[] tr =new ArrayList[];//
 
     @Override
-    public void init(DirectedWeightedGraph g)
-    {
-         this._g = g;
+    public void init(DirectedWeightedGraph g) {
+        this._g = g;
     }
 
     @Override
-    public DirectedWeightedGraph getGraph()
-    {
+    public DirectedWeightedGraph getGraph() {
         return _g;
     }
 
     @Override
-    public DirectedWeightedGraph copy()
-    {
+    public DirectedWeightedGraph copy() {
         // create a new graph
         DirectedWeightedGraph newG = new Graph();
 
         // copy all nodes
         Iterator<NodeData> itrN = _g.nodeIter();
-        while (itrN.hasNext())
-        {
-           NodeData toCopy = itrN.next();
-           int key = toCopy.getKey();
+        while (itrN.hasNext()) {
+            NodeData toCopy = itrN.next();
+            int key = toCopy.getKey();
             NodeData newN = new Node(key);
-           if(toCopy.getLocation() != null)
-           {
+            if (toCopy.getLocation() != null) {
                 GeoLocation newGl = new Geo_Location(toCopy.getLocation().x(), toCopy.getLocation().y(), toCopy.getLocation().z());
                 newN.setLocation(newGl);
-           }
-           else
-           {
-               newN.setLocation(null);
-           }
-           double newW = toCopy.getWeight();
-           String Info = toCopy.getInfo();
-           int tag = toCopy.getTag();
-           newN.setWeight(newW);
-           newN.setInfo(Info);
-           newN.setTag(tag);
-           newG.addNode(newN);
+            } else {
+                newN.setLocation(null);
+            }
+            double newW = toCopy.getWeight();
+            String Info = toCopy.getInfo();
+            int tag = toCopy.getTag();
+            newN.setWeight(newW);
+            newN.setInfo(Info);
+            newN.setTag(tag);
+            newG.addNode(newN);
         }
 
         // copy all Edges
         Iterator<EdgeData> iterE = _g.edgeIter();
-        while (iterE.hasNext())
-        {
+        while (iterE.hasNext()) {
             EdgeData e = iterE.next();
-            newG.connect(e.getSrc(), e.getDest(),e.getWeight());
+            newG.connect(e.getSrc(), e.getDest(), e.getWeight());
         }
 
         return newG;
     }
 
+    //BFS by Gabriel nivash
     @Override
-    public boolean isConnected() {
+    public boolean isConnected()
+    {
+        int blackCounter = 0;
+        if(_g.nodeSize() == 0) {return true;} // or false?
 
-        return false;
+        DirectedWeightedGraph copyG = cleanColors(copy());
+
+        Iterator<NodeData> iterN = copyG.nodeIter() ;
+
+        while (iterN.hasNext())
+        {
+            NodeData startNode = iterN.next();
+
+            // insert start node to queue
+            Queue<NodeData> q = new LinkedList<NodeData>();
+            q.add(startNode);
+
+            while (!q.isEmpty())
+            {
+                NodeData u = q.remove();
+                Iterator<EdgeData> itr = copyG.edgeIter(u.getKey());
+                while (itr.hasNext())
+                {
+                    NodeData neighborOfU = copyG.getNode(itr.next().getDest());
+                    if (neighborOfU.getTag() == WHITE)
+                    {
+                        neighborOfU.setTag(GRAY);
+                        q.add(neighborOfU);
+                    }
+                }
+                u.setTag(BLACK);
+                blackCounter++;
+            }
+            if (_g.nodeSize() != blackCounter) {return false;}
+            blackCounter = 0;
+            cleanColors(copyG);
+        }
+        return true;
     }
 
     @Override
@@ -98,4 +127,18 @@ public class GraphAlgo implements DirectedWeightedGraphAlgorithms{
     public boolean load(String file) {
         return false;
     }
+
+    private DirectedWeightedGraph cleanColors(DirectedWeightedGraph g)
+    {
+        Iterator<NodeData> itr = g.nodeIter();
+        while (itr.hasNext())
+        {
+            NodeData node = itr.next();
+            node.setTag(WHITE);
+        }
+        return g;
+    }
+
 }
+
+
--- Main.java
+++ Main.java
@@ -23,70 +23,36 @@ public class Main {
         G.connect(4,3,3);
         G.connect(4,5,3.5);
 
-        GraphAlgo algos = new GraphAlgo();
-        algos.init(G);
-
-        //GraphAlgo newAlgo = new GraphAlgo();
-        Graph copiOfG = (Graph)algos.copy();
-
-
-
-
-
         G.printNodes();
         System.out.println();
         G.printEdeges();
         System.out.println("Edges number : " + G.edgeSize());
         System.out.println("Nodes  number : " + G.nodeSize());
-
-        System.out.println();
-        // remove edge 3-->1
-        G.removeEdge(3, 1);
-        G.printNodes();
-        System.out.println();
-        G.printEdeges();
-        System.out.println("Edges number : " + G.edgeSize());
-        System.out.println("Nodes  number : " + G.nodeSize());
-
-        // delete node3
-        NodeData re1 = G.removeNode(3);
-        G.printNodes();
-        System.out.println();
-        G.printEdeges();
-        System.out.println("Edges number : " + G.edgeSize());
-        System.out.println("Nodes  number : " + G.nodeSize());
-
         System.out.println();
 
-        // delete edge4_5
-        EdgeData re2 = G.removeEdge(4,5);
-        G.printNodes();
-        System.out.println();
-        G.printEdeges();
-        System.out.println("Edges number : " + G.edgeSize());
-        System.out.println("Nodes  number : " + G.nodeSize());
-
-        System.out.println();
-
-        Iterator<EdgeData> iter = G.edgeIter();
-        while(iter.hasNext())
-        {
-            EdgeData ed = iter.next();
-            int src = ed.getSrc();
-            int dst = ed.getDest();
-            double w = ed.getWeight();
-            System.out.println(src + "---(" + w + ")--> " + dst);
-        }
+        //-----------check connected----------------
+        DirectedWeightedGraphAlgorithms algorithms = new GraphAlgo();
+        algorithms.init(G);
+        boolean isConnectedGraph = algorithms.isConnected();
+        System.out.println("connected? " + isConnectedGraph);
 
 
-        //---------------print grapg like first lines -----------------
-        copiOfG.printNodes();
-        System.out.println();
-        copiOfG.printEdeges();
-        System.out.println("Edges number : " + copiOfG.edgeSize());
-        System.out.println("Nodes  number : " + copiOfG.nodeSize());
+        Graph G2 = new Graph();
+        Node n21 = new Node(1);
+        Node n22 = new Node(2);
+        Node n23 = new Node(3);
 
-//        //System.out.println(src + "---(" + w + ")--> " + dst);
+        G2.addNode(n21);
+        G2.addNode(n22);
+        G2.addNode(n23);
+        G2.connect(1,2,0.5);
+        G2.connect(2,3,0.5);
+        G2.connect(3,1,0.5);
+        algorithms.init(G2);
+        G2.printNodes();
+        G2.printEdeges();
+        isConnectedGraph = algorithms.isConnected();
+        System.out.println("connected? " + isConnectedGraph);
 
 
 
@@ -94,16 +60,60 @@ public class Main {
 
 
 
+        Graph G3 = new Graph();
+        Node n31 = new Node(1);
+        Node n32 = new Node(2);
+        Node n33 = new Node(3);
+        Node n34 = new Node(4);
+        Node n35 = new Node(5);
+        System.out.println("-------------------------------------------------------------------------------------------------");
+        G3.addNode(n31);
+        G3.addNode(n32);
+        G3.addNode(n33);
+        G3.addNode(n34);
+        G3.addNode(n35);
+        G3.connect(1,2,0.5);
+        G3.connect(2,3,2.5);
+        G3.connect(3,1,4.5);
+        G3.connect(3,4,0.5);
+        G3.connect(4,5,2);
+        G3.connect(5,1,6.5);
+        algorithms.init(G3);
+        G3.printNodes();
+        G3.printEdeges();
+        isConnectedGraph = algorithms.isConnected();
+        System.out.println("connected?  " + isConnectedGraph);
+
+        Graph G4 = new Graph();
+        Node n41 = new Node(1);
+        Node n42 = new Node(2);
+        Node n43 = new Node(3);
+        Node n44 = new Node(4);
+        Node n45 = new Node(5);
+        G4.addNode(n41);
+        G4.addNode(n42);
+        G4.addNode(n43);
+        G4.addNode(n44);
+        G4.addNode(n45);
+        G4.connect(1,2,0.5);
+        G4.connect(1,3,10.5);
+        G4.connect(2,3,2.5);
+        G4.connect(3,1,4.5);
+        G4.connect(3,4,0.5);
+        G4.connect(3,5,8.5);
+        G4.connect(4,5,2);
+        G4.connect(5,1,6.5);
+        G4.connect(5,2,9);
+        G4.connect(5,4,7);
+        System.out.println("-------------------------------------------------------------------------------------------------");
+        algorithms.init(G4);
+        G4.printNodes();
+        G4.printEdeges();
+        isConnectedGraph = algorithms.isConnected();
+        System.out.println("connected?  " + isConnectedGraph);
 
 
-
-
-
-
-
-
-
-
+        //------------------------------------------
 
 
     }
