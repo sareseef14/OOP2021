414ea57ed884e6962c720eb42eaeaf6d7ce4ceed
sareseef14 <95385314+sareseef14@users.noreply.github.com>
2021-12-12 23:24:30 +0200

    Add files via upload

--- GraphAlgo.java
+++ GraphAlgo.java
@@ -1,54 +1,82 @@
 package api;
-
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.*;
+import com.google.gson.*;
 
 public class GraphAlgo implements DirectedWeightedGraphAlgorithms {
     final int WHITE = 0;
     final int GRAY = 1;
     final int BLACK = 2;
     private DirectedWeightedGraph _g;
-    //  private static ArrayList<EdgeData>[] adj =new ArrayList[];//
-    //private static ArrayList<EdgeData>[] tr =new ArrayList[];//
 
     @Override
-    public void init(DirectedWeightedGraph g) {
+    public void init(DirectedWeightedGraph g)
+    {
         this._g = g;
     }
 
     @Override
-    public DirectedWeightedGraph getGraph() {
+    public DirectedWeightedGraph getGraph()
+    {
         return _g;
     }
 
     @Override
-    public DirectedWeightedGraph copy() {
+    public DirectedWeightedGraph copy()
+    {
         // create a new graph
         DirectedWeightedGraph newG = new Graph();
 
         // copy all nodes
         Iterator<NodeData> itrN = _g.nodeIter();
-        while (itrN.hasNext()) {
+        while (itrN.hasNext())
+        {
+            // take the current node
             NodeData toCopy = itrN.next();
+
+            // take from current node his key
             int key = toCopy.getKey();
+
+            // create new node with the same key.
             NodeData newN = new Node(key);
-            if (toCopy.getLocation() != null) {
+
+            // take copy of location into new node.
+            if (toCopy.getLocation() != null)
+            {
                 GeoLocation newGl = new Geo_Location(toCopy.getLocation().x(), toCopy.getLocation().y(), toCopy.getLocation().z());
                 newN.setLocation(newGl);
-            } else {
+            }
+            else
+            {
                 newN.setLocation(null);
             }
+
+            // set the same width
             double newW = toCopy.getWeight();
-            String Info = toCopy.getInfo();
-            int tag = toCopy.getTag();
             newN.setWeight(newW);
+
+            // set the same Info
+            String Info = toCopy.getInfo();
             newN.setInfo(Info);
+
+            // set the same tag
+            int tag = toCopy.getTag();
             newN.setTag(tag);
+
+            // add the new deep copied node to the grath.
             newG.addNode(newN);
         }
 
         // copy all Edges
         Iterator<EdgeData> iterE = _g.edgeIter();
-        while (iterE.hasNext()) {
+        while (iterE.hasNext())
+        {
             EdgeData e = iterE.next();
             newG.connect(e.getSrc(), e.getDest(), e.getWeight());
         }
@@ -61,7 +89,7 @@ public class GraphAlgo implements DirectedWeightedGraphAlgorithms {
     public boolean isConnected()
     {
         int blackCounter = 0;
-        if(_g.nodeSize() == 0) {return true;} // or false?
+        if(_g.nodeSize() == 0) {return false;} // or true?
 
         DirectedWeightedGraph copyG = cleanColors(copy());
 
@@ -99,34 +127,407 @@ public class GraphAlgo implements DirectedWeightedGraphAlgorithms {
     }
 
     @Override
-    public double shortestPathDist(int src, int dest) {
-        return 0;
+    public double shortestPathDist(int src, int dest)
+    {
+        HashMap<Integer, Double> dist = new HashMap<Integer, Double>();
+        HashMap<Integer, Integer> prnt = new HashMap<Integer, Integer>();
+        PriorityQueue<Integer> Q = new PriorityQueue<Integer>((a, b) -> (int)(dist.get(a) - dist.get(b)));
+        PriorityQueue<Integer> Q2 = new PriorityQueue<Integer>((a, b) -> (int)(dist.get(a) - dist.get(b)));
+
+        Iterator<NodeData> itrN = _g.nodeIter();
+        while(itrN.hasNext())
+        {
+            NodeData n = itrN.next();
+            if(n.getKey() == src)
+            {
+                dist.put(src, 0.0);
+            }
+            else
+            {
+                dist.put(n.getKey(), Double.MAX_VALUE);
+            }
+            prnt.put(n.getKey(), -1);
+            Q.add(n.getKey());
+        }
+
+        while(Q.size() > 0)
+        {
+            int u = Q.poll();
+            //System.out.print(u +  " ");
+
+            Iterator<EdgeData> itrE = _g.edgeIter(u);
+            while(itrE.hasNext())
+            {
+                EdgeData u_v = itrE.next();
+                int v = u_v.getDest();
+
+                if(dist.get(v) > dist.get(u) + u_v.getWeight())
+                {
+                    dist.replace(v, dist.get(u) + u_v.getWeight());
+                    prnt.replace(v, u);
+
+                    // exactly like decrease key
+                    while(!Q.isEmpty())
+                    {
+                        Q2.add(Q.poll());
+                    }
+                    while (!Q2.isEmpty())
+                    {
+                        Q.add(Q2.poll());
+                    }
+                }
+            }
+
+            if(u == dest){break;}
+        }
+
+        return dist.get(dest) == Double.MAX_VALUE ? -1 : dist.get(dest);
     }
 
     @Override
-    public List<NodeData> shortestPath(int src, int dest) {
-        return null;
+    public List<NodeData> shortestPath(int src, int dest)
+    {
+        HashMap<Integer, Double> dist = new HashMap<Integer, Double>();
+        HashMap<Integer, Integer> prnt = new HashMap<Integer, Integer>();
+        PriorityQueue<Integer> Q = new PriorityQueue<Integer>((a, b) -> (int)(dist.get(a) - dist.get(b)));
+        PriorityQueue<Integer> Q2 = new PriorityQueue<Integer>((a, b) -> (int)(dist.get(a) - dist.get(b))); //
+
+        Iterator<NodeData> itrN = _g.nodeIter();
+        while(itrN.hasNext())
+        {
+            NodeData n = itrN.next();
+            if(n.getKey() == src)
+            {
+                dist.put(src, 0.0);
+            }
+            else
+            {
+                dist.put(n.getKey(), Double.MAX_VALUE);
+            }
+            prnt.put(n.getKey(), -1);
+            Q.add(n.getKey());
+        }
+
+        while(Q.size() > 0)
+        {
+            int u = Q.poll();
+
+            Iterator<EdgeData> itrE = _g.edgeIter(u);
+            while(itrE.hasNext())
+            {
+                EdgeData u_v = itrE.next();
+                int v = u_v.getDest();
+
+                if(dist.get(v) > dist.get(u) + u_v.getWeight())
+                {
+                    dist.replace(v, dist.get(u) + u_v.getWeight());
+                    prnt.replace(v, u);
+
+                    // exactly like "decrease key" :)
+                    while(!Q.isEmpty())
+                    {
+                        Q2.add(Q.poll());
+                    }
+                    while (!Q2.isEmpty())
+                    {
+                        Q.add(Q2.poll());
+                    }
+                }
+            }
+            if(u == dest){break;} // when we finish with our dest.
+        }
+
+        // if no path
+        if(dist.get(dest) == Double.MAX_VALUE)
+        {
+            return null;
+        }
+
+        List<NodeData> path = new LinkedList<NodeData>();
+        int curr = dest;
+        path.add(0, _g.getNode(curr));
+        while(curr != src)
+        {
+            int prev = prnt.get(curr);
+            path.add(0, _g.getNode(prev));
+            curr = prev;
+        }
+
+        return path;
     }
 
     @Override
-    public NodeData center() {
-        return null;
+    public NodeData center()
+    {
+        if (!this.isConnected())
+        {
+            return null;
+        }
+
+        HashMap<Integer, Double> shortestPathSum = new HashMap<Integer, Double>();
+        Iterator<NodeData> it = _g.nodeIter();
+        while(it.hasNext())
+        {
+            NodeData curr = it.next();
+            shortestPathSum.put(curr.getKey(), Double.MAX_VALUE);
+        }
+
+        it = _g.nodeIter();
+        int returnCenter = it.next().getKey();
+        System.out.println("\nCurrCenter: " + returnCenter);
+
+        Iterator<NodeData> itSrc = _g.nodeIter();
+        while(itSrc.hasNext())
+        {
+            int src = itSrc.next().getKey();
+            Iterator<NodeData> itDst = _g.nodeIter();
+            {
+                while (itDst.hasNext())
+                {
+                    int dst = itDst.next().getKey();
+                    double pathLenSrcToDst = shortestPathDist(src, dst);
+                    if(shortestPathSum.get(src) == Double.MAX_VALUE)
+                    {
+                        shortestPathSum.replace(src, pathLenSrcToDst);
+                    }
+                    else
+                    {
+                        shortestPathSum.replace(src, shortestPathSum.get(src) + pathLenSrcToDst);
+                    }
+                }
+            }
+            if (shortestPathSum.get(returnCenter) > shortestPathSum.get(src))
+            {
+                System.out.println("\nCurrCenter: " + src);
+                returnCenter = src;
+            }
+
+        }
+
+        System.out.println("\n"+ shortestPathSum.toString());
+        return _g.getNode(returnCenter);
     }
 
     @Override
-    public List<NodeData> tsp(List<NodeData> cities) {
-        return null;
+    public List<NodeData> tsp(List<NodeData> cities)
+    {
+        HashSet<Integer> notVisited = new HashSet<Integer>();
+        List<NodeData> pathAroundAllCities = new LinkedList<NodeData>();
+        Iterator<NodeData> itCities = cities.iterator();
+        while(itCities.hasNext())
+        {
+            NodeData n = itCities.next();
+            if(pathAroundAllCities.size() == 0)
+            {
+                pathAroundAllCities.add(n);
+            }
+            else
+            {
+                notVisited.add(n.getKey());
+            }
+        }
+
+        while(!notVisited.isEmpty())
+        {
+            int src = pathAroundAllCities.get(pathAroundAllCities.size()-1).getKey();
+            Iterator<Integer> itNotVisit = notVisited.iterator();
+            int dst = itNotVisit.next();
+            List<NodeData> srcToDstPath = shortestPath(src, dst);
+
+            Iterator<NodeData> itSrcToDstPath = srcToDstPath.iterator();
+            itSrcToDstPath.next(); // avoid from first element because it exits at and of pathAroundAllCitiesList!
+            while (itSrcToDstPath.hasNext())
+            {
+                NodeData n = itSrcToDstPath.next();
+                pathAroundAllCities.add(n); // add to path
+                notVisited.remove(n.getKey()); // remove from 'not visited'.
+            }
+
+        }
+
+        return pathAroundAllCities;
     }
 
     @Override
-    public boolean save(String file) {
-        return false;
+    public boolean save(String file)
+    {
+
+        // serializer for nodes.
+        JsonSerializer<NodeData> serializerNodes = new JsonSerializer<NodeData>()
+        {
+            @Override
+            public JsonElement serialize(NodeData nodeData, Type type, JsonSerializationContext jsonSerializationContext)
+            {
+                JsonObject obj = new JsonObject();
+                GeoLocation gl = nodeData.getLocation();
+                String pos = gl.x() + "," + gl.y() + "," + gl.z();
+                obj.addProperty("pos", pos);
+                obj.addProperty("id", nodeData.getKey());
+
+                return obj;
+            }
+        };
+
+        // serializer for edges.
+        JsonSerializer<EdgeData> serializerEdges = new JsonSerializer<EdgeData>()
+        {
+            @Override
+            public JsonElement serialize(EdgeData edgeData, Type type, JsonSerializationContext jsonSerializationContext)
+            {
+                JsonObject obj = new JsonObject();
+                obj.addProperty("src", edgeData.getSrc());
+                obj.addProperty("w", edgeData.getWeight());
+                obj.addProperty("dest", edgeData.getDest());
+                return obj;
+            }
+        };
+
+        // serializer for graph.
+        JsonSerializer<DirectedWeightedGraph> serializerGraph = new JsonSerializer<DirectedWeightedGraph>()
+        {
+            @Override
+            public JsonElement serialize(DirectedWeightedGraph G, Type type, JsonSerializationContext jsonSerializationContext)
+            {
+                // list for edges
+                Iterator<EdgeData> itE = G.edgeIter();
+                ArrayList<EdgeData> arrE = new ArrayList<EdgeData>();
+                while(itE.hasNext())
+                {
+                    arrE.add(itE.next());
+                }
+
+                // list for nodes
+                Iterator<NodeData> itN = G.nodeIter();
+                ArrayList<NodeData> arrN = new ArrayList<NodeData>();
+                while(itN.hasNext())
+                {
+                    arrN.add(itN.next());
+                }
+
+                Gson gsonNodes = new GsonBuilder().registerTypeAdapter(Node.class, serializerNodes).create();
+                Gson gsonEdges = new GsonBuilder().registerTypeAdapter(Edge.class, serializerEdges).create();
+
+                JsonObject obj = new JsonObject();
+                obj.add("Edges", gsonEdges.toJsonTree(arrE));
+                obj.add("Nodes", gsonNodes.toJsonTree(arrN));
+
+                return obj;
+            }
+        };
+
+//        // list for nodes
+//        Iterator<NodeData> itN = _g.nodeIter();
+//        ArrayList<NodeData> arrN = new ArrayList<NodeData>();
+//        while(itN.hasNext())
+//        {
+//            arrN.add(itN.next());
+//        }
+
+//        Gson gson = new GsonBuilder().registerTypeAdapter(Node.class, serializerNodes).create();
+//        String jsonNodesStr = gson.toJson(arrN);
+//        System.out.println(jsonNodesStr);
+
+//        // list for edges
+//        Iterator<EdgeData> itE = _g.edgeIter();
+//        ArrayList<EdgeData> arrE = new ArrayList<EdgeData>();
+//        while(itE.hasNext())
+//        {
+//            arrE.add(itE.next());
+//        }
+
+//        gson = new GsonBuilder().registerTypeAdapter(Edge.class, serializerEdges).create();
+//        String jsonEdgesStr = gson.toJson(arrE);
+//        System.out.println(jsonEdgesStr);
+
+        Gson gson = new GsonBuilder().registerTypeAdapter(Graph.class, serializerGraph).setPrettyPrinting().create();
+        String jsonGraphStr = gson.toJson(this._g);
+
+        try
+        {
+            FileWriter fw = new FileWriter(file);
+            fw.write(jsonGraphStr);
+            fw.close();
+        }
+        catch (IOException e){return  false;}
+
+        return true;
     }
 
     @Override
-    public boolean load(String file) {
-        return false;
+    public boolean load(String file)
+    {
+        JsonDeserializer<DirectedWeightedGraph> deserializerGraph = new JsonDeserializer<DirectedWeightedGraph>()
+        {
+            @Override
+            public DirectedWeightedGraph deserialize(JsonElement jsonElement, Type type, JsonDeserializationContext context) throws JsonParseException
+            {
+                DirectedWeightedGraph newGraph = new Graph();
+
+                // for Nodes.
+                JsonObject jo = jsonElement.getAsJsonObject();
+                JsonArray arrJO = jo.get("Nodes").getAsJsonArray();
+                Iterator<JsonElement> itJO = arrJO.iterator();
+                while(itJO.hasNext())
+                {
+                    JsonObject je = (JsonObject) itJO.next();
+
+                    // take location
+                    String pos = je.get("pos").toString();
+                    String xyz[] = pos.split(",");
+                    double x = Double.parseDouble(xyz[0].split("\"")[1]);
+                    double y = Double.parseDouble(xyz[1]);
+                    double z = Double.parseDouble(xyz[2].split("\"")[0]);
+                    GeoLocation newLocation = new Geo_Location(x, y, z);
+
+                    // take key
+                    int key = je.get("id").getAsInt();
+
+                    // create Node
+                    NodeData newNode = new Node(key);
+                    newNode.setLocation(newLocation);
+                    newGraph.addNode(newNode);
+                }
+
+
+                // for Edges.
+                arrJO = jo.get("Edges").getAsJsonArray();
+                itJO = arrJO.iterator();
+                while(itJO.hasNext())
+                {
+                    JsonObject je = (JsonObject) itJO.next();
+
+                    // take src
+                    int src = je.get("src").getAsInt();
+
+                    // take width
+                    double width = je.get("w").getAsDouble();
+
+                    // take dest
+                    int dst = je.get("dest").getAsInt();
+
+                    newGraph.connect(src, dst, width);
+                }
+
+                return newGraph;
+            }
+        };
+
+        String content = "";
+        try
+        {
+            content = Files.readString(Path.of(file));
+            //System.out.println(content);
+        }
+        catch (IOException e){return false;}
+
+        Gson gson = new GsonBuilder().registerTypeAdapter(Graph.class, deserializerGraph).setPrettyPrinting().create();
+        DirectedWeightedGraph newGraph = gson.fromJson(content, Graph.class);
+        this.init(newGraph);
+        return true;
     }
+
+
+    //----------HELPERS-------------
 
     private DirectedWeightedGraph cleanColors(DirectedWeightedGraph g)
     {
@@ -140,5 +541,7 @@ public class GraphAlgo implements DirectedWeightedGraphAlgorithms {
     }
 
 }
+
+
 
 
--- Main.java
+++ Main.java
@@ -1,125 +1,16 @@
 package api;
 
-import java.util.Hashtable;
-import java.util.Iterator;
-import java.util.PriorityQueue;
-import java.util.Queue;
-
-public class Main {
-    public static void main(String[] args) {
-        Graph G = new Graph();
-        Node n1=new Node(1);
-        Node n2= new Node(2);
-        Node n3= new Node(3);
-        Node n4= new Node(4);
-        Node n5= new Node(5);
-        G.addNode(n1);
-        G.addNode(n2);
-        G.addNode(n3);
-        G.addNode(n4);
-        G.addNode(n5);
-        G.connect(1,2,0.5);
-        G.connect(2,4,2);
-        G.connect(3,1,1.5);
-        G.connect(3,2,1);
-        G.connect(3,4,2.5);
-        G.connect(4,3,3);
-        G.connect(4,5,3.5);
-
-        G.printNodes();
-        System.out.println();
-        G.printEdeges();
-        System.out.println("Edges number : " + G.edgeSize());
-        System.out.println("Nodes  number : " + G.nodeSize());
-        System.out.println();
-
-        //-----------check connected----------------
-        DirectedWeightedGraphAlgorithms algorithms = new GraphAlgo();
-        algorithms.init(G);
-        boolean isConnectedGraph = algorithms.isConnected();
-        System.out.println("connected? " + isConnectedGraph);
+import javax.swing.JFrame;
+import java.awt.*;
 
 
-        Graph G2 = new Graph();
-        Node n21 = new Node(1);
-        Node n22 = new Node(2);
-        Node n23 = new Node(3);
+public class Main
+{
 
-        G2.addNode(n21);
-        G2.addNode(n22);
-        G2.addNode(n23);
-        G2.connect(1,2,0.5);
-        G2.connect(2,3,0.5);
-        G2.connect(3,1,0.5);
-        algorithms.init(G2);
-        G2.printNodes();
-        G2.printEdeges();
-        isConnectedGraph = algorithms.isConnected();
-        System.out.println("connected? " + isConnectedGraph);
-
-
-
-
-
-
-
-        Graph G3 = new Graph();
-        Node n31 = new Node(1);
-        Node n32 = new Node(2);
-        Node n33 = new Node(3);
-        Node n34 = new Node(4);
-        Node n35 = new Node(5);
-        System.out.println("-------------------------------------------------------------------------------------------------");
-        G3.addNode(n31);
-        G3.addNode(n32);
-        G3.addNode(n33);
-        G3.addNode(n34);
-        G3.addNode(n35);
-        G3.connect(1,2,0.5);
-        G3.connect(2,3,2.5);
-        G3.connect(3,1,4.5);
-        G3.connect(3,4,0.5);
-        G3.connect(4,5,2);
-        G3.connect(5,1,6.5);
-        algorithms.init(G3);
-        G3.printNodes();
-        G3.printEdeges();
-        isConnectedGraph = algorithms.isConnected();
-        System.out.println("connected?  " + isConnectedGraph);
-
-        Graph G4 = new Graph();
-        Node n41 = new Node(1);
-        Node n42 = new Node(2);
-        Node n43 = new Node(3);
-        Node n44 = new Node(4);
-        Node n45 = new Node(5);
-        G4.addNode(n41);
-        G4.addNode(n42);
-        G4.addNode(n43);
-        G4.addNode(n44);
-        G4.addNode(n45);
-        G4.connect(1,2,0.5);
-        G4.connect(1,3,10.5);
-        G4.connect(2,3,2.5);
-        G4.connect(3,1,4.5);
-        G4.connect(3,4,0.5);
-        G4.connect(3,5,8.5);
-        G4.connect(4,5,2);
-        G4.connect(5,1,6.5);
-        G4.connect(5,2,9);
-        G4.connect(5,4,7);
-        System.out.println("-------------------------------------------------------------------------------------------------");
-        algorithms.init(G4);
-        G4.printNodes();
-        G4.printEdeges();
-        isConnectedGraph = algorithms.isConnected();
-        System.out.println("connected?  " + isConnectedGraph);
-
-        Queue<DirectedWeightedGraph> h= new PriorityQueue<>();
-
-        //------------------------------------------
-        Hashtable<Integer, Double> ht1 = new Hashtable<>();
-        ht1.
-
+    public static void main(String[] args)
+    {
+        Window window =new Window();
+        window.setVisible(true);
     }
+
 }
